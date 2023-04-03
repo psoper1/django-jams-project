46a49ca154a80cbfdf7354f60bca4435d9858ebe
reeman9000 <preston.soper@student.awesomeinc.org>
2023-04-03 21:51:37 +0000

    connected models to admin

--- jams/djangojams/admin.py
+++ jams/djangojams/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Genre
+from .models import Album
+from .models import Artist
+from .models import Song
+from .models import Playlist
 
 # Register your models here.
+
+admin.site.register(Genre)
+admin.site.register(Album)
+admin.site.register(Artist)
+admin.site.register(Song)
+admin.site.register(Playlist)
