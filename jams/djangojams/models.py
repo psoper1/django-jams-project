14a09eda6be827ee4021d7ddad1776decd77b5ad
reeman9000 <preston.soper@student.awesomeinc.org>
2023-04-03 21:02:42 +0000

    made album model

--- jams/djangojams/models.py
+++ jams/djangojams/models.py
@@ -10,3 +10,14 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
+
+class Album(models.Model):
+    name = models.CharField(max_length=500, null=False)
+    publish_date = models.DateField(null=True)
+    cover_art = models.URLField(max_length=300, null=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
